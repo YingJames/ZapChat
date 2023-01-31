7bfd05b2bb7c66ae998b727dcb71e078593a6d54
James Ying <jyab0617@gmail.com>
2023-01-31 10:13:41 -0500

    add func to store new user details to firestore

--- src/components/SignInOut.jsx
+++ src/components/SignInOut.jsx
@@ -1,10 +1,30 @@
 import firebase from 'firebase/compat/app';
-import { auth } from '../firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+
+async function registerNewUser(userDetails) {
+  await setDoc(doc(db, "users", userDetails.uid), {
+    uid: userDetails.uid,
+    displayName: userDetails.displayName,
+    email: userDetails.email,
+    photoURL: userDetails.photoURL,
+  });
+};
 
 export function SignIn() {
+
   const signInWithGoogle = () => {
+
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
+    auth.signInWithPopup(provider).then(result => {
+      let isNewUser = result.additionalUserInfo.isNewUser;
+      if (isNewUser) {
+        registerNewUser(result.user);
+      } else {
+        console.log(`Existing user: ${result.user}`);
+      }
+      
+    })
   }
 
   return (
